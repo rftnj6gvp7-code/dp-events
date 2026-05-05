84acf03288f0de21889c19b5064f04b3e9892e10
bertrandcadet-6527 <bertrand.cadet@mac.com>
2026-05-05 22:18:26 +0200

    fix cookies production

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,17 +2,18 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore = cookies() as any
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() { return cookieStore.getAll() },
+        getAll() {
+          return cookies().getAll()
+        },
         setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }: any) =>
-              cookieStore.set(name, value, options)
+              (cookies() as any).set(name, value, options)
             )
           } catch {}
         },
